a8a7833aead6611d2471e05861075adfe3f38b07
Zeyad-3amr <zeyad.3amr01@gmail.com>
2025-01-16 12:54:07 +0200

    end of section 8

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Authorize]
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    {
+        var users = await userRepository.GetMembersAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
+    {
+        var user = await userRepository.GetMemberAsync(username);
+        if (user == null) return NotFound();
+        return (user);
+    }
+}
